5bbe4825b88c63e942131b249bd54cea2a35b38c
d1son <dson120@gmail.com>
2016-01-21 19:36:55 -0500

    created word.js file and made the word constructor function

---
+++ word.js
@@ -0,0 +1,10 @@
+var letter = require("./letter.js");
+
+var Word = function(wrd){
+	this.word = wrd,
+	this.lets = [],
+	this.found = false,
+	this.getLets = function(){
+		for(var i = 0; i < word.length; i++)
+	}
+}
