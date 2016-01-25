83c83e5d8e48217f17dc9fcb83d5e992cdd1ba04
d1son <dson120@gmail.com>
2016-01-25 16:03:31 -0500

    added prompt npm package

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ prompt.start() // this is required to use the prompt for npm
 
 var game = { // create the game object with keys inside
 	wordBank :["Broncos", "Panthers", "Warriors", "Lakers", "ESPN", "Football", "Basketball"] //insterted random wordbank words 
-	this.guessRemaining = 10; //this. refers to the object it is a part of now, so .this in this case is referring to the variable/array "game"
+	this.guessRemaining = 10;
 	this.currentWrd = null;
 	this.startGame = function(wrd){
 		var randWord = Math.floor(Math.random() * this.wordBank.length)
@@ -42,7 +42,7 @@ var game = { // create the game object with keys inside
 			else{
 				console.log(self.currentWrd.wordRender()); // followed the instructions on learner nation, not sure if it will work
 			}
-		})
+		});
 	}
 }
 
