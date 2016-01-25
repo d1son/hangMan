d9ee11aedf4ff2361e5d13de1e45c83aa11267eb
d1son <dson120@gmail.com>
2016-01-25 15:33:36 -0500

    created keepPromptingUser function, not sure if its working

--- main.js
+++ main.js
@@ -8,6 +8,17 @@ var game = { // create the game object with keys inside
 	this.guessRemaining = 10; //this. refers to the object it is a part of now, so .this in this case is referring to the variable/array "game"
 	this.currentWrd = null;
 	this.startGame = function(wrd){
-		
+		var randWord = Math.floor(Math.random() * this.wordBank.length)
+		this.currentWrd = new Word(this.wordBank[randWord]);
+		this.currentWrd.getLets(); //not too sure here
+		this.keepPromptingUser; //not too sure here
+	}
+	this.keepPromptingUser : function(){
+		var self = this; //self will now hold the value 'this' in order to keep it in scope
+		prompt.get(["guessLetter"], function(err, result){
+			console.log("The letter or space you guessed is " + result.guessLetter);
+			var findHowManyOfUserGuess = currentWrd.checkIfLetterFound(result.guessLetter)
+		})
 	}
 }
+
