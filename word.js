0e5d4f03388e9a135313efa2b13b88dec0b9a813
d1son <dson120@gmail.com>
2016-01-25 12:46:32 -0500

    added chechIfLetterFound function, tocheck user input against set word

--- word.js
+++ word.js
@@ -10,4 +10,15 @@ var Word = function(wrd){
 			this.lets.push(new Letter(this.word[i]));
 		}
 	}
+	this.checkIfLetterFound = function(guessLetter){
+		var whatToReturn = 0;
+		for(var i = 0; i < this.lets.length; i++){
+			// if the value of lets[i] === the letter the user guessed, set .appear to true, add +1 to whatToReturn
+			if (this.lets[i] === guessLetter) {
+				this.lets[i].appear = true;
+				whatToReturn++;
+			}
+		}
+		return whatToReturn;
+	}
 }
